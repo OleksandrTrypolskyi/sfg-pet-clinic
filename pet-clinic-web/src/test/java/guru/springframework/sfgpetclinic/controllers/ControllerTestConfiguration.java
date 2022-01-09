8dbb2c866f776095ea9577bec55385adff8321ef
TrypolskyiS <tripolskys@gmail.com>
2022-01-09 09:56:56 +0200

    update test config

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTestConfiguration.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTestConfiguration.java
@@ -1,9 +1,23 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("guru.springframework.sfgpetclinic.controllers")
 public class ControllerTestConfiguration {
+
+    @Bean
+    IndexController indexController() {
+        return new IndexController();
+    }
+
+    @Bean
+    OwnerController ownerController() {
+        return new OwnerController();
+    }
+
+    @Bean
+    VetController vetController() {
+        return new VetController();
+    }
 }
