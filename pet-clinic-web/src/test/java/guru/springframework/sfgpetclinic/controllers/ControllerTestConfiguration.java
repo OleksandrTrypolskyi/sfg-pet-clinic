2db69b32dc6ceae52260eb1645e55a565a8c49f7
TrypolskyiS <tripolskys@gmail.com>
2022-01-13 14:53:57 +0200

    added Pets and Contact info to owners on bootstrap. closes #32

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTestConfiguration.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTestConfiguration.java
@@ -1,8 +1,12 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
+import guru.springframework.sfgpetclinic.services.map.PetServiceMap;
+import guru.springframework.sfgpetclinic.services.map.PetTypeServiceMap;
 import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,8 +30,18 @@ public class ControllerTestConfiguration {
     }
 
     @Bean
-    OwnerService ownerService() {
-        return new OwnerServiceMap();
+    OwnerService ownerService(PetTypeService petTypeService, PetService petService) {
+        return new OwnerServiceMap(petTypeService, petService);
+    }
+
+    @Bean
+    PetService petService() {
+        return new PetServiceMap();
+    }
+
+    @Bean
+    PetTypeService petTypeService() {
+        return new PetTypeServiceMap();
     }
 
     @Bean
