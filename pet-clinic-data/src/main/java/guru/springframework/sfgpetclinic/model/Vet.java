f07b5fb972e3182cfabdc556f28e7f4e95c85e75
TrypolskyiS <tripolskys@gmail.com>
2022-01-18 09:39:14 +0200

    updated vet with JPA annotations. closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,20 +1,27 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
-    private Set<Speciality> specialities;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+    private Set<Speciality> specialties;
 
     public Vet() {
-        this.specialities = new HashSet<>();
+        this.specialties = new HashSet<>();
     }
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
+    public Set<Speciality> getSpecialties() {
+        return specialties;
     }
 
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
+    public void setSpecialties(Set<Speciality> specialties) {
+        this.specialties = specialties;
     }
 }
