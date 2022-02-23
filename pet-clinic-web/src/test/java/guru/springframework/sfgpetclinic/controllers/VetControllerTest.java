b3e749e4ebc720ad0c67adc85490a82afbeb96ad
TrypolskyiS <tripolskys@gmail.com>
2022-02-23 11:23:14 +0200

    Implement Vets JSON EndPoint. Closes #64

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -6,14 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -34,7 +29,7 @@ class VetControllerTest {
 
     @Test
     void listVets() throws Exception {
-        mockMvc.perform(get("/vets.html"))
+        mockMvc.perform(get("/vets"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeHasNoErrors())
                 .andExpect(model().attributeExists("vets"))
